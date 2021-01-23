1c1025e089a059464d1b2628eb6a91c36eda43d6
GrantLi1012 <304948612@qq.com>
2021-01-23 15:32:21 -0800

    build folder

--- snapary-frontend/src/pages/History/History.js
+++ snapary-frontend/src/pages/History/History.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './History.css'
+import DatePicker from "react-datepicker";
+import ListGroup from "react-bootstrap/ListGroup"
+import "react-datepicker/dist/react-datepicker.css";
 
 function History() {
+    const [date, setDate] = useState(new Date());
+    const history = ['I am happy', 'Meh ok', 'Died'];
+    const listitems = history.map((entry) => 
+        <ListGroup.Item action variant="info">
+            entry
+        </ListGroup.Item>);
+
     return (
         <>
             <div className="history-bg">
-                <h1>History Page</h1>
+                <div className="title-row">
+                    <div className="title-col">
+                        <div className="title-line">
+                            View Your Diaries
+                        </div>
+                    </div>
+                    <div className="title-col">
+                        <div className="select-line">
+                            Select Date
+                        </div>
+                        <DatePicker style={{"paddingTop":"1rem"}} selected={date} onChange={date => setDate(date)} />
+                    </div>
+                </div>
+                <div className="list-group">
+                    <ListGroup>
+                        {listitems}
+                    </ListGroup>
+                </div>
             </div>
         </>
     )
