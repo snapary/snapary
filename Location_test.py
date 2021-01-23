0587123360853e3e1db3f7ee9e5d28350143b645
Sam Gu <gupangmk2@gmail.com>
2021-01-23 16:37:03 +0800

    add firestore section to Location_test

--- Location_test.py
+++ Location_test.py
@@ -1,4 +1,7 @@
 from Location import Location as LC
+import sys
+import firestore
+from datetime import datetime
 
 coordinate_list = [[51.10182014819713, -114.17764491534524],
                    [49.255711088513465, -123.18431855101835],
@@ -8,10 +11,21 @@ coordinate_list = [[51.10182014819713, -114.17764491534524],
                    [49.238280829778404, -122.97387388324273],
                    [49.24264037099486, -122.93537749094077]]
 
+
+def print_entry(username, time, longitude, latitude, area):
+    print({u'time': time, u'longitude': longitude, u'latitude': latitude, u'area': area})
+
+
 for coordinate in coordinate_list:
     location = LC(coordinate[0], coordinate[1])
     print(location.get_raw_location())
-    print(location.get_area())
+    area = location.get_area()
+    if sys.argv[0]:
+        print_entry("grant1", datetime.now().strftime("%d/%m/%Y %H:%M:%S"), coordinate[0], coordinate[1], area)
+        firestore.save_entry("grant1", datetime.now().strftime("%d/%m/%Y %H:%M:%S"), coordinate[0], coordinate[1], area)
+
+
+
 
 
 
--- firestore.py
+++ firestore.py
@@ -16,6 +16,19 @@ def make_user(username, hash, salt):
             'history': []
         })
         return True
-    
+
+
+def save_entry(username, time, longitude, latitude, area):
+    user_page = firestore.Client().collection(u'Users').document(str(username).lower())
+    if user_page.get().exists:
+        user_page.update({u'history' : firestore.ArrayUnion([{u'time': time,
+                                                              u'longitude': longitude,
+                                                              u'latitude': latitude,
+                                                              u'area': area}])})
+        return True
+    else:
+        # user does not exist
+        return False
+
 if __name__ == '__main__':
     pass
