bef0823d26ca05b7d27d78f26537fdbc9b64089c
GrantLi1012 <304948612@qq.com>
2021-01-23 19:39:56 -0800

    map

--- snapary-frontend/src/components/Maps/MapContainer.js
+++ snapary-frontend/src/components/Maps/MapContainer.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
  
-export class MapContainer extends Component {
+function MapContainer(props) {
+    const [lat, setlat] = useState(0);
+    const [lgn, setlgn] = useState(0);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            latitude: 0,
-            longtitude: 0
-        };
-    }
-
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(function(position) {
+                console.log("Latitude is :", position.coords.latitude);
+                console.log("Longitude is :", position.coords.longitude);
+                setlat(position.coords.latitude);
+                setlgn(position.coords.longitude);
+            });
+        }
+     }, []);
+     /*
     componentDidMount() {
         var newState = {};
         if (navigator.geolocation) {
@@ -22,8 +27,11 @@ export class MapContainer extends Component {
                     longtitude: position.coords.longitude
                 }
             });
-            this.setState(newState);
         }
+        if (newState) {
+            console.log(newState);
+            this.setState(newState);
+        }*/
         /*navigator.geolocation.getCurrentPosition(function(position) {
             console.log(position.coords.latitude);
             var newState = {
@@ -31,25 +39,22 @@ export class MapContainer extends Component {
                 longtitude: position.coords.longitude
             }
             this.setState(newState);
-        });*/
-    }
-
-    render() {
-        const style = {
+        });    }*/
+    
+    const style = {
             width: '42rem',
             height: '39rem',
             border: '5px dashed pink'
         }
-        return (
-            <div>
-                <Map google={this.props.google} zoom={14} containerStyle={style}
-                     center={{lat: this.state.latitude, lng:this.state.longtitude}}>
-                <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
-                </Map>
-            </div>
-        );
-    }
+
+    return (
+        <div>
+            <Map google={props.google} zoom={14} containerStyle={style}
+                 center={{lat: lat, lng:lgn}}>
+            <Marker />
+            </Map>
+        </div>
+    );
 }
  
 export default GoogleApiWrapper({
