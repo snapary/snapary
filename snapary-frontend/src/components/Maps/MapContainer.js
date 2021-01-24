1423b277062fadadc35486de04c2068777820686
GrantLi1012 <304948612@qq.com>
2021-01-23 19:04:36 -0800

    added get geolocation

--- snapary-frontend/src/components/Maps/MapContainer.js
+++ snapary-frontend/src/components/Maps/MapContainer.js
@@ -2,6 +2,38 @@ import React, { Component } from 'react'
 import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
  
 export class MapContainer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            latitude: 0,
+            longtitude: 0
+        };
+    }
+
+    componentDidMount() {
+        var newState = {};
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(function(position) {
+                console.log("Latitude is :", position.coords.latitude);
+                console.log("Longitude is :", position.coords.longitude);
+                newState = {
+                    latitude: position.coords.latitude,
+                    longtitude: position.coords.longitude
+                }
+            });
+            this.setState(newState);
+        }
+        /*navigator.geolocation.getCurrentPosition(function(position) {
+            console.log(position.coords.latitude);
+            var newState = {
+                latitude: position.coords.latitude,
+                longtitude: position.coords.longitude
+            }
+            this.setState(newState);
+        });*/
+    }
+
     render() {
         const style = {
             width: '42rem',
@@ -10,7 +42,8 @@ export class MapContainer extends Component {
         }
         return (
             <div>
-                <Map google={this.props.google} zoom={14} containerStyle={style}>
+                <Map google={this.props.google} zoom={14} containerStyle={style}
+                     center={{lat: this.state.latitude, lng:this.state.longtitude}}>
                 <Marker onClick={this.onMarkerClick}
                     name={'Current location'} />
                 </Map>
